431d4712c6a365fba3711ed43dcd0c46a5cb57a9
danbylgakov79 <93380109+danbylgakov79@users.noreply.github.com>
2022-09-28 21:26:20 +0300

    Initial commit

---
+++ src/main/java/com/example/bonchan/BonchanApplication.java
@@ -0,0 +1,13 @@
+package com.example.bonchan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonchanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BonchanApplication.class, args);
+    }
+
+}
